b58438931707742c5bd2f00a2bd95f47336ffff3
tuankiet091002 <tuankiet091002@gmail.com>
2024-05-29 08:36:38 +0700

    finish reply function

--- src/main/java/com/java/java_proj/dto/request/forupdate/URequestMessageStatus.java
+++ src/main/java/com/java/java_proj/dto/request/forupdate/URequestMessageStatus.java
@@ -1,13 +1,11 @@
 package com.java.java_proj.dto.request.forupdate;
 
 import com.java.java_proj.entities.enums.MessageStatusType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 
 @AllArgsConstructor
@@ -19,6 +17,6 @@ public class URequestMessageStatus {
     @NotNull(message = "Message id is required.")
     private Integer id;
 
-    @NotBlank(message = "Message status is required.")
+    @NotNull(message = "Message status is required.")
     private MessageStatusType status;
 }
